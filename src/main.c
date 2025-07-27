c3c3045a173e546d2d94c4ef14fc52079eb28ff3
avneeshlingwal9 <avaneeshlingwal@gmail.com>
2025-07-27 16:34:44 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -456,7 +456,7 @@ int execute(int fd , char** arguments , int numArgs){
 
 	else if(command == PSYNC){
 
-		char* response = "FULLRESYNC8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb0";
+		char* response = "FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0";
 
 		char* toSend = encodeStr(response);
 
